6be05141b99eb36fa1caf5d934b6ba8521843954
sbrills <stevenbrills@hotmail.com>
2022-11-01 16:05:47 -0400

    started work on the forward simulator

---
+++ src/forward_simulator.cpp
@@ -0,0 +1,17 @@
+#include<vector>
+
+// Forward simulation function that takes initial configuration and a piecewise linear path
+// as an input
+
+std::vector<double> forward_simulator(
+    std::vector<double> q_init,
+    std::vector<std::vector<double>> piecewise_linear
+){
+
+    // Find the intersection points of the look
+
+}
+
+
+
+//
