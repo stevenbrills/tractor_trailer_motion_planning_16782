88a3830d5d565e4ba26d111ea77aa5c359a59335
sbrills <stevenbrills@hotmail.com>
2022-11-02 14:36:05 -0400

    Added lookahead circle intersection with tractor and trailer lookaheads

--- src/forward_simulator.cpp
+++ src/forward_simulator.cpp
@@ -1,5 +1,7 @@
 #include <vector>
 #include <stdexcept>
+#include <cmath>
+#include <iostream>
 
 // Forward simulation function that takes initial configuration and a piecewise linear path
 // as an input
@@ -22,13 +24,154 @@ double trailer_wheelbase = 0.8;
 // Trailer hitch offset
 double tractor_m = 0.2;
 
-static void find_intersection_point(
-    std::vector<double>& q_current, 
-    std::vector<std::vector<double>>& piecewise_linear, 
-    int& id_start, 
-    int& id_goal, 
+bool check_double_equal(
+    double& a,
+    double& b
+){
+    double epsilon = 0.001;
+
+    if (abs(a-b)<=epsilon){
+        return true;
+    }
+
+    return false;
+
+}
+
+static bool find_intersection_point(
+    const std::vector<double>& q_current, 
+    const std::vector<std::vector<double>>& piecewise_linear, 
+    const int& id_start, 
+    const int& id_goal,
+    const bool& is_forward,
     std::vector<double>& intersection_point
 ){
+
+    // if the motion is reverse, use the trailer centerd lookahead circle
+    if (!is_forward){
+
+        // Set up co-efficients for quadratic equation
+        double a = pow((piecewise_linear[1][0] - piecewise_linear[0][0]),2) + 
+        pow((piecewise_linear[1][1] - piecewise_linear[0][1]),2);
+
+        double b = 2*(
+            ((piecewise_linear[0][0] - q_current[0])*(piecewise_linear[1][0] - piecewise_linear[0][0])) + 
+            ((piecewise_linear[0][1] - q_current[1])*(piecewise_linear[1][1] - piecewise_linear[0][1]))
+        );
+
+        double c = pow((piecewise_linear[0][0] - q_current[0]),2) + pow((piecewise_linear[0][1] - q_current[1]),2) - backward_lookahead_radius;
+
+        double det = pow(b,2) - 4*a*c;
+
+        if(det < 0){
+            std::cout << "No real roots" << std::endl;
+        }
+
+        std::cout << "Determinant value could be near zero and fail equality, check performance " << std::endl;
+        std::cout << "Determinant value: " << det << std::endl;
+
+
+        if(det>=0){
+
+            double t1 = ((-1*b) + sqrt(det))/(2*a);
+            double t2 = ((-1*b) - sqrt(det))/(2*a);
+
+            if (check_double_equal(t1, t2)){
+
+                intersection_point[0] = (piecewise_linear[1][0]*t1) + (1-t1)*piecewise_linear[0][0];
+                intersection_point[1] = (piecewise_linear[1][1]*t1) + (1-t1)*piecewise_linear[0][1];
+                return true; // Intersection detected
+
+            }
+            else{
+
+                // t1 will always be greater than t2 and since the second point of the piecewise linear
+                // line will be the direction of travel, using t1 should give the point to drive to
+
+                double x_intersection1 = (piecewise_linear[1][0]*t1) + (1-t1)*piecewise_linear[0][0];
+                double y_intersection1 = (piecewise_linear[1][1]*t1) + (1-t1)*piecewise_linear[0][1];
+
+                double x_intersection2 = (piecewise_linear[1][0]*t2) + (1-t2)*piecewise_linear[0][0];
+                double y_intersection2 = (piecewise_linear[1][1]*t2) + (1-t2)*piecewise_linear[0][1];
+
+                intersection_point[0] = (piecewise_linear[1][0]*t1) + (1-t1)*piecewise_linear[0][0];
+                intersection_point[1] = (piecewise_linear[1][1]*t1) + (1-t1)*piecewise_linear[0][1];
+
+                return true;  //intersection detected
+
+            }
+
+        }
+        else{
+            std::cout << "No intersections detected" << std::endl;
+        }
+
+    }
+
+    // If the motion is forward, use tractor lookahead circle
+    else{
+
+        // Set up co-efficients for quadratic equation
+        double a = pow((piecewise_linear[1][0] - piecewise_linear[0][0]),2) + 
+        pow((piecewise_linear[1][1] - piecewise_linear[0][1]),2);
+
+        double b = 2*(
+            ((piecewise_linear[0][0] - q_current[4])*(piecewise_linear[1][0] - piecewise_linear[0][0])) + 
+            ((piecewise_linear[0][1] - q_current[5])*(piecewise_linear[1][1] - piecewise_linear[0][1]))
+        );
+
+        double c = pow((piecewise_linear[0][0] - q_current[0]),2) + pow((piecewise_linear[0][1] - q_current[1]),2) - backward_lookahead_radius;
+
+        double det = pow(b,2) - 4*a*c;
+
+        if(det < 0){
+            std::cout << "No real roots" << std::endl;
+        }
+
+        std::cout << "Determinant value could be near zero and fail equality, check performance " << std::endl;
+        std::cout << "Determinant value: " << det << std::endl;
+
+
+        if(det>=0){
+
+            double t1 = ((-1*b) + sqrt(det))/(2*a);
+            double t2 = ((-1*b) - sqrt(det))/(2*a);
+
+            if (check_double_equal(t1, t2)){
+
+                intersection_point[0] = (piecewise_linear[1][0]*t1) + (1-t1)*piecewise_linear[0][0];
+                intersection_point[1] = (piecewise_linear[1][1]*t1) + (1-t1)*piecewise_linear[0][1];
+                return true; // Intersection detected
+
+            }
+            else{
+
+                // t1 will always be greater than t2 and since the second point of the piecewise linear
+                // line will be the direction of travel, using t1 should give the point to drive to
+
+                double x_intersection1 = (piecewise_linear[1][0]*t1) + (1-t1)*piecewise_linear[0][0];
+                double y_intersection1 = (piecewise_linear[1][1]*t1) + (1-t1)*piecewise_linear[0][1];
+
+                double x_intersection2 = (piecewise_linear[1][0]*t2) + (1-t2)*piecewise_linear[0][0];
+                double y_intersection2 = (piecewise_linear[1][1]*t2) + (1-t2)*piecewise_linear[0][1];
+
+                intersection_point[0] = (piecewise_linear[1][0]*t1) + (1-t1)*piecewise_linear[0][0];
+                intersection_point[1] = (piecewise_linear[1][1]*t1) + (1-t1)*piecewise_linear[0][1];
+
+                return true;  //intersection detected
+
+            }
+
+        }
+        else{
+            std::cout << "No intersections detected" << std::endl;
+        }
+
+    }
+
+
+    return false;
+
 
     
     
