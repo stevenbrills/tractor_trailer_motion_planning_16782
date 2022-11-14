0e91ce769f7562242fd110133d6c4893c74ad7c1
naren <stark710@github.com>
2022-11-14 00:14:22 -0500

    added visualizer code for visualizing map

---
+++ src/visualizer.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def parseFiileandCreateArray(filename):
+    map_int = []
+    with open(filename) as f:
+        lines = f.readlines()
+        h = lines[0].split(' ')[1]
+        w = lines[1].split(' ')[1]
+        map_str = lines[2:]
+        map_int = []
+        for line in map_str:
+            map_int.append([int(x) for x in line.split(' ') if x != '\n'])
+    #convert map_int to numpy array
+    map_int = np.array(map_int)
+    #diplay map_int as image
+    plt.imshow(map_int)
+    plt.show()
+
+
+parseFiileandCreateArray('/home/naren/catkin_cl_rrt/tractor_trailer_motion_planning_16782/maps/map1.txt')
